0f5713cc8acfebb6e396ce7668d955dfae4b65a7
Martin Stottmeister <mstottmeister@mmm.com>
2021-06-21 14:26:30 +0200

    course1 part1

---
+++ course1/print_variables.py
@@ -0,0 +1,46 @@
+a = 7
+b = 56.5
+print(a)
+print(b)
+print(a+b)
+
+print("----------------------------------")
+
+c="Text"
+print(c)
+
+print("----------------------------------")
+
+d = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(d)
+print(d["brand"])
+
+
+print("----------------------------------")
+
+e = ["Ford", "Volvo", "BMW"]
+print(e)
+print(e[0])
+print(e[1])
+print(e[2])
+
+
+print("----------------------------------")
+
+f= [["Ford", "Volvo", "BMW"],["slow", "fast", "superfast"]]
+print(f)
+print(f[0])
+print(f[0][1])
+print(f[1])
+print(f[1][1])
+
+
+print("----------------------------------")
+
+#print(a + c) # does not work!
+print(str(a)+c)
+print(a,c, "test")
