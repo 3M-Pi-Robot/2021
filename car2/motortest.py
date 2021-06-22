1601b082789b7b6c8bafb5c1f3eafbcb3c0a6cdb
Martin Stottmeister <mstottmeister@mmm.com>
2021-06-22 15:17:40 +0200

    car2

---
+++ car2/motortest.py
@@ -0,0 +1,17 @@
+import pin
+import time
+
+pin.load("config.json")
+
+pin.Out("IN1",1) # forward
+pin.Out("IN2",0)
+pin.Out("ENA",1)
+time.sleep(5)
+
+pin.Out("IN1",0) # backward
+pin.Out("IN2",1)
+pin.Out("ENA",1)
+time.sleep(5)
+
+pin.Out("ENA",0) # stop
+pin.cleanup()
