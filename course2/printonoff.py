7f56e17d64a6a4b4bf99464593b4a7d36a7d9f25
Martin Stottmeister <mstottmeister@mmm.com>
2021-06-22 09:55:32 +0200

    printonoff

---
+++ course2/printonoff.py
@@ -0,0 +1,13 @@
+# function / module
+
+def first(input):
+    if(input==1):
+        print("On")
+    else:
+        print("Off")
+
+def second():
+    print("test")
+              
+first(1)
+second()
